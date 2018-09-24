540732ebee6bfacdf69bdfb4e2176a8c66000d6a
HatataToshihiro <ee53071@meiji.ac.jp>
2018-09-24 18:08:03 +0900

    update

--- src/grass2grid.cpp
+++ src/grass2grid.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <limits>
 #include <sensor_msgs/Image.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <cv_bridge/cv_bridge.h>
@@ -10,22 +11,31 @@ sensor_msgs::Image seg_img;
 sensor_msgs::Image depth_img;
 nav_msgs::OccupancyGrid map;
 
+const int pixel_num_x = 513;
+const int pixel_num_y = 288;
+bool seg_flag = false;
 int grid[400][400] = {-1};
-int seg[288][513][3] = {0};
-bool seg_ok[288][513] = {false};
-float depth[288][513] = {0};
 
+
+struct Pixel{
+    int seg[3];
+    bool seg_ok;
+    float depth;
+    float rad_x;
+    float rad_y;
+};
+
+Pixel pixel[pixel_num_y][pixel_num_x];
 
 cv::Mat image_s;
 cv::Mat image_d;
-
 
 bool im_s_s = false;
 void seg_callback(const sensor_msgs::ImageConstPtr& msg)
 {
     seg_img = *msg;
     image_s = cv_bridge::toCvCopy(seg_img, sensor_msgs::image_encodings::BGR8)->image;
-    //resize(image_s,image_s,cv::Size(),320/513.,180/288.);
+    //resize(image_s,image_s,cv::Size(),320/pixel_num_x.,180/pixel_num_y.);
     
     cv::imshow("image_s",image_s);
     if(!im_s_s){
@@ -38,20 +48,21 @@ void seg_callback(const sensor_msgs::ImageConstPtr& msg)
     //printf("height = %d\n",image_s.height);
     //printf("width = %d\n",image_s.width);
 
-    for(int y=0;y<288;y++){
-        for(int x=0;x<513;x++){
+    for(int y=0;y<pixel_num_y;y++){
+        for(int x=0;x<pixel_num_x;x++){
             for(int c=0;c<3;c++){
-                seg[y][x][c] = image_s.at<cv::Vec3b>(y,x)[c];
+                pixel[y][x].seg[c] = image_s.at<cv::Vec3b>(y,x)[c];
             }
         }
     }
-    for(int y=0;y<288;y++){
-        for(int x=0;x<513;x++){
-            if( seg[y][x][0]==0 && seg[y][x][1]==0 && seg[y][x][2]==(0||128) )seg_ok[y][x]=true;
+    for(int y=0;y<pixel_num_y;y++){
+        for(int x=0;x<pixel_num_x;x++){
+            if( (pixel[y][x].seg[0]==0) && (pixel[y][x].seg[1]==0) && ((pixel[y][x].seg[2]==0) || pixel[y][x].seg[2]==128)) pixel[y][x].seg_ok=true;
+            else pixel[y][x].seg_ok=false;
         }
     }
 
-
+    seg_flag = true;
     cv::waitKey(1);
     /*
     [B,G,R]=[0,0,128],[0,0,0]
@@ -67,24 +78,96 @@ void depth_callback(const sensor_msgs::ImageConstPtr& msg)
     map.header = depth_img.header;
 
     image_d = cv_bridge::toCvCopy(depth_img, sensor_msgs::image_encodings::TYPE_32FC1)->image;
-    resize(image_d,image_d,cv::Size(),513/320.,288/180.);
+    resize(image_d,image_d,cv::Size(),pixel_num_x/320.,pixel_num_y/180.);
 
-    cv::imshow("image_d",image_d);
-    if(!im_d_s){
-        cv::moveWindow("image_d",640,40);
-        im_d_s = true;
-    }
+    //cv::imshow("image_d",image_d);
+    //if(!im_d_s){
+    //    cv::moveWindow("image_d",640,40);
+    //    im_d_s = true;
+    //}
 
     //printf("image_d[90,160] = %f\n",image_d.at<float>(90,160));//(y,x)
     //printf("height = %d\n",depth_img.height);
     //printf("width = %d\n",depth_img.width);
 
-    for(int y=0;y<288;y++){
-        for(int x=0;x<513;x++){
-            depth[y][x] = image_d.at<float>(y,x);
+    for(int y=0;y<pixel_num_y;y++){
+        for(int x=0;x<pixel_num_x;x++){
+            pixel[y][x].depth = image_d.at<float>(y,x);
         }
     }
     cv::waitKey(1);
+}
+
+float deg2rad(float deg)
+{
+    float rad = deg*M_PI/180;
+    return rad;
+}
+
+void store_angle(float angle_x,float angle_y)
+{
+    float dx = angle_x/pixel_num_x;
+    float dy = angle_y/pixel_num_y;
+    float init_x = angle_x/2;
+    float init_y = angle_y/2;
+    for(int x=0;x<pixel_num_x;x++){
+        for(int y=0;y<pixel_num_y;y++){
+            pixel[y][x].rad_x = deg2rad(dx*x - init_x);
+            pixel[y][x].rad_y = deg2rad(dy*y - init_y);
+        }
+    }
+    printf("rad[0][0] = (%.2f, %.2f)\n",pixel[0][0].rad_x,pixel[0][0].rad_y);
+    printf("rad[287][512] = (%.2f, %.2f)\n",pixel[287][512].rad_x,pixel[287][512].rad_y);
+}
+
+void init_grid(void)
+{
+    for(int y=0;y<map.info.height;y++){
+        for(int x=0;x<map.info.width;x++){
+            grid[y][x] = 0;
+        }
+    }
+}
+
+void store_mapdata(void)
+{
+    int count = 0;
+    for(int x=map.info.width-1;x>=0;x--){
+        for(int y=map.info.height-1;y>=0;y--){
+            //printf("%d\n",y*w+x);
+            map.data[count] = grid[y][x];
+            count++;
+        }
+    }
+}
+
+void calc_object(void)
+{
+    
+    for(int y=0;y<pixel_num_y;y++){
+        for(int x=0;x<pixel_num_x;x++){
+            if(!std::isnan(pixel[y][x].depth) && pixel[y][x].depth <= 30.0){
+                int ob_x = (pixel[y][x].depth*cos(pixel[y][x].rad_y)*sin(pixel[y][x].rad_x) / map.info.resolution);
+                int ob_y = (pixel[y][x].depth*cos(pixel[y][x].rad_y)*cos(pixel[y][x].rad_x) / map.info.resolution);
+                printf("before %d,%d\n",ob_x,ob_y);
+                if(ob_y<=map.info.width/2.0){
+                            printf("after1 %d,%d\n",ob_x,ob_y);
+                    if(map.info.width/2.0>=-ob_x){
+                            printf("after2 %d,%d\n",ob_x,ob_y);
+                        if(ob_x<=map.info.width/2.0){
+                            printf("after3 %d,%d\n",ob_x,ob_y);
+                            if(!pixel[y][x].seg_ok){
+                                grid[(int)map.info.height/2 - ob_y][(int)map.info.width/2 + ob_x] = 100;
+                            }else{
+                                grid[(int)map.info.height/2 - ob_y][(int)map.info.width/2 + ob_x] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("-\n");
 }
 
 int main(int argc, char** argv)
@@ -114,23 +197,19 @@ int main(int argc, char** argv)
 
     ros::Rate loop_rate(10);
 
+    store_angle(102.5, 70.0);
+
     while(ros::ok()){
 
-        //printf("grass2grid\n");
-        //ここからメイン処理（grid[][]に情報を入れる）
+        if(seg_flag){
+            init_grid();
 
+            calc_object();
+            seg_flag = false;
 
-
-        //grid[][]の情報をmap.dataに入れる
-        for(int y=0;y<map.info.height;y++){
-            for(int x=0;x<map.info.width;x++){
-                //printf("%d\n",y*map.info.width+x);
-                map.data[(y*map.info.width) + x] = grid[y][x];
-            }
+            store_mapdata();
+            map_pub.publish(map);
         }
-
-        map_pub.publish(map);
-
         ros::spinOnce();
         loop_rate.sleep();
     }
